fb1e7f97fc1f9dc2ea2fc61baab265ed9283d90d
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-01-31 10:33:58 -0300

    teste

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,4 +1,4 @@
-import { SignUpController } from './signup.ts'
+import { SignUpController } from './signup'
 
 describe('SignUp Controller', () => {
   test('Should return 400 if no name is provided', () => {
