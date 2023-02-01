50b00aa6b23c28429e6987767ca624849945b348
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-02-01 13:22:15 -0300

    refactor: add customized error for missing param

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,11 +1,12 @@
 import { type HttpRequest, type HttpResponse } from '../protocols/http'
+import { MissingParamErrors } from '../errors/missing-params-errors'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
     if (!httpRequest.body.name) {
       return {
         statusCode: 400,
-        body: new Error('Missing param: name')
+        body: new MissingParamErrors('name')
       }
     }
 
